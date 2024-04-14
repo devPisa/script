c63d4177a98e8cd275fe673610c0bbca5ee1b53c
devPisa <cesar.au563@gmail.com>
2024-04-14 00:03:01 -0300

    Remoção da estrutura de repetição que estava quebrando o código

--- cadastros/main.py
+++ cadastros/main.py
@@ -1,34 +1,28 @@
 from Empresa import Empresa
 from Usuario import Usuario
 from Resposta import Resposta
-from conexao_db import conexao_db
-def main():
-    conn = conexao_db()
-    if conn:
-        while True:
-            decisao = int(input("""
-                                Projeto Integrador SAGA - Grupo 2
-                                Oque deseja fazer?
 
-                                1 - Cadastrar Empresa
-                                2 - Cadastrar Usuario
-                                3 - Cadastrar Resposta
-                                """
-                                ))
-            print()
-            if decisao == 1:
-                Empresa().inserirBanco()
-                break
-            elif decisao == 2:
-                Usuario().inserirBanco()
-                break
-            elif decisao == 3:
-                Resposta().inserirBanco()
-                break
-            else:
-                print("Favor inserir opção valida, 1 a 3.")
+def main():
+    decisao = input("""
+                    Projeto Integrador SAGA - Grupo 2
+                    Oque deseja fazer?
+
+                    1 - Cadastrar Empresa
+                    2 - Cadastrar Usuario
+                    3 - Cadastrar Resposta
+                    """
+                    )
     
+    decisao = int(decisao)
+
+    if decisao == 1:
+        Empresa().inserirBanco()
+    elif decisao == 2:
+        Usuario().inserirBanco()
+    elif decisao == 3:
+        Resposta().inserirBanco()
     else:
-        print("Falha ao conectar ao banco")
+        print("Favor inserir opção valida, 1 a 3.")
+
 if __name__ == "__main__":
     main()
